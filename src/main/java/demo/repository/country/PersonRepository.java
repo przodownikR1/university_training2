e140246e542cfffa9e8f47a3081b9abc9c8b759a
Slawomir Borowiec <przodownik@tlen.pl>
2015-05-16 00:20:23 +0200

    personRepository added method

--- src/main/java/demo/repository/country/PersonRepository.java
+++ src/main/java/demo/repository/country/PersonRepository.java
@@ -1,14 +1,13 @@
 package demo.repository.country;
 
-import java.io.Serializable;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import demo.domain.Person;
-import java.lang.String;
-import java.util.List;
-@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+
+@RestResource(path = "people")
 public interface PersonRepository extends JpaRepository<Person, Long>{
    List<Person> findByName(String name);
 }
